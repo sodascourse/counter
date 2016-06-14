71a29d0b6f665806bb7afe0a0857bb3e030750c1
sodastsai <sodas2002@gmail.com>
2016-06-15 01:12:26 +0800

    Add first version

---
+++ Counter/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Copyright 2016 Tien-Che Tsai
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var currentCount: Int = 0
+
+    @IBOutlet weak var countLabel: UILabel!
+
+    @IBAction func addCount(sender: UIButton) {
+        self.currentCount += 1
+        self.countLabel.text = String(self.currentCount)
+    }
+
+    @IBAction func resetCounter(sender: UIButton) {
+        self.currentCount = 0
+        self.countLabel.text = "0"
+    }
+
+}
